b57d6aef81b2eaf16433b8ea22430eeec8710edc
ddieppa <ddieppad@live.com>
2020-04-01 02:15:22 -0400

    Fix failing unit testings. use CollectionAssert instead of assert

--- HackerRank.UnitTests/LeftRotationTests.cs
+++ HackerRank.UnitTests/LeftRotationTests.cs
@@ -1,5 +1,9 @@
-using HackerRank.Solutions;
+﻿using HackerRank.Solutions;
+using Microsoft.VisualBasic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace HackerRank.UnitTests
 {
@@ -7,57 +11,53 @@ namespace HackerRank.UnitTests
     public class LeftRotationTests
     {
         private LeftRotation _solution;
-        
+
         [TestInitialize]
         public void TestInitialize()
         {
             _solution = new LeftRotation();
         }
 
-        [TestCleanup]
-        public void testClean()
-        {
-            _solution = null;
-        }
-
         [TestMethod]
-        public void RotateLeft_ShoulRotate_2Times()
+        public void RotateLeft_Scenario1_Rotate2Times()
         {
             //Arrange
-            int[] firstArray = new[] {1, 2, 3, 4, 5};
+            int[] firstArray = new int[] { 1, 2, 3, 4, 5 };
             int rotations = 2;
-            int[] expected = new[] {3, 4, 5, 1, 2};
+            int[] expected = new int[] { 3, 4, 5, 1, 2 };
+
             //Act
-            int[] actual = _solution.RotateLeft(firstArray, rotations);
-                
+            var actual = _solution.RotateLeft(firstArray, rotations);
+
             //Assert
-            Assert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected, actual);
         }
+
         [TestMethod]
         public void RotateLeft_ShouldRotate_4Times()
         {
             //Arrange
-            int[] firstArray = new[] {1, 2, 3, 4, 5};
+            int[] firstArray = new[] { 1, 2, 3, 4, 5 };
             int rotations = 4;
-            int[] expected = new[] {5, 1, 2, 3, 4};
+            int[] expected = new[] { 5, 1, 2, 3, 4 };
             //Act
             int[] actual = _solution.RotateLeft(firstArray, rotations);
-                
+
             //Assert
-            Assert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected, actual);
         }
         [TestMethod]
         public void RotateLeft_ShouldRotate_10Times()
         {
             //Arrange
-            int[] firstArray = new[] {41 ,73 ,89, 7 ,10 ,1 ,59 ,58, 84 ,77 ,77, 97 ,58, 1 ,86 ,58 ,26 ,10, 86 ,51};
+            int[] firstArray = new[] { 41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51 };
             int rotations = 10;
-            int[] expected = new[] {77, 97 ,58 ,1 ,86 ,58 ,26, 10, 86 ,51, 41 ,73, 89 ,7 ,10 ,1, 59, 58, 84, 77};
+            int[] expected = new[] { 77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77 };
             //Act
             int[] actual = _solution.RotateLeft(firstArray, rotations);
-                
+
             //Assert
-            Assert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
