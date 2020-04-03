dc5b30c446b4867653945ec488c4659315b79d60
ddieppa <ddieppad@live.com>
2020-04-03 13:03:16 -0400

    Change Solution name and folder structure

---
+++ HackerRank.Solutions/PsychometricTestting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Solutions
+{
+    public class PsychometricTestting
+    {
+        public int[] JobOffers(int[] scores, int[] lowerLimits, int[] upperLimits)
+        {
+            int[] result = new int[lowerLimits.Length];
+
+            for (int i = 0; i < lowerLimits.Length; i++)
+            {
+                for (int score = 0; score < scores.Length; score++)
+                {
+                    if (lowerLimits[i] <= scores[score] && scores[score] <= upperLimits[i])
+                    {
+                        result[i] += 1;
+                    }
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackerRank.UnitTests/PsychometricTestingTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.UnitTests
+{
+    [TestClass]
+    public class PsychometricTestingTests
+    {
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void JobOffers_Scenario1_Return()
+        {
+
+
+        }
+    }
+}
