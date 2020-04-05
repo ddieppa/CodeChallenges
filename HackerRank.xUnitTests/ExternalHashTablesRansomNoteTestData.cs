4c4e999ed57ddf2157d73e1b29ba08adabe57ffe
Daniel Dieppa <ddieppad@live.com>
2020-04-05 14:07:46 -0400

    Add xUnit and all posible ways to unit test

---
+++ HackerRank.xUnitTests/ExternalHashTablesRansomNoteTestData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit.Sdk;
+
+namespace HackerRank.xUnitTests
+{
+    public class ExternalHashTablesRansomNoteTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                var fileLines =
+                    File.ReadAllLines("HashTablesRansomNoteTestData.txt");
+                var testCases = new List<object[]>();
+                foreach (var line in fileLines)
+                {
+                    var values = line.Split(',');
+                    object[] testCase =
+                    {
+                        values[0], values[1].Split(' '), values[2].Split(' ')
+                    };
+                    testCases.Add(testCase);
+                }
+
+                return testCases;
+            }
+        }
+    }
+}
---
+++ HackerRank.xUnitTests/InternalHashTablesRansomNoteTestData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank.xUnitTests
+{
+    public class InternalHashTablesRansomNoteTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    "Yes"
+                    , "give me One grand today night".Split(' ')
+                    , "give One grand today".Split(' ')
+                };
+                yield return new object[]
+                {
+                    "Yes"
+                    , "apgo clm w lxkvg mwz elo bg elo lxkvg elo apgo apgo w elo bg"
+                        .Split(' ')
+                    , "elo lxkvg bg mwz clm w".Split(' ')
+                };
+                yield return new object[]
+                {
+                    "No", "two times three is not four", "two times two is four"
+                };
+                yield return new object[]
+                {
+                    "No"
+                    , "two times three is not four".Split(' ')
+                    , "two times two is four".Split(' ')
+                };
+                yield return new object[]
+                {
+                    "No"
+                    , "give me One grand today night".Split(' ')
+                    , "give one grand today".Split(' ')
+                };
+            }
+        }
+    }
+}
